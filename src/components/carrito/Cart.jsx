7c6ed89025c82bda6dae98e94312cb6473d6e308
Camilo Betancur <betancurcamilo50@gmail.com>
2022-11-18 23:04:30 -0500

    se termino la clase de los contextos

--- src/components/carrito/Cart.jsx
+++ src/components/carrito/Cart.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+//import { useContext } from 'react'
+//import { CartContext } from '../../contexto/CartContext'
 
 const Cart = () => {
+        //const {AddToCart} = useContext(CartContext)
   return (
     <div>Aqui ira el carrito de compras</div>
   )
--- src/contexto/CartContext.jsx
+++ src/contexto/CartContext.jsx
@@ -6,13 +6,30 @@ const CartProvider = (props) =>{
         const [cart, setCart] = useState([]);
        
        let AddToCart = (producto , cantidad) =>{
-                setCart([...cart , {...producto , cantidad}])
+                if (isInCart(producto.id)) {
+                        alert('Ya esta en el carrito sumale la cantidad flaco ')
+                } else {
+                        setCart([...cart , {...producto , cantidad}])
+                }
        }
+
+        //funcion para ver si esta en el carrito
+       const isInCart = (id) =>{
+                return cart.some((prod)=>prod.id===id)
+       }
+
+       //funcion para vaciar el carrito
+
+       const deleteAll = () =>{
+                setCart([])
+       }
+
+ 
 
        console.log(cart)
 
         return(
-                <CartContext.Provider value={{cart , AddToCart}}>
+                <CartContext.Provider value={{cart , AddToCart, deleteAll}}>
                 {props.children}
                 </CartContext.Provider>
         );
